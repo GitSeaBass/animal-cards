9a3c2cdf251517fd2c04e1f512ee5deef2d2cf4f
Sebastian Garcia <sag44402@uga.edu>
2023-07-10 13:09:20 -0400

    basic grid for packs

--- Front/animal-cards/src/App.js
+++ Front/animal-cards/src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <div className='main'>
           <div className='packs'>
+            
             <div className='pack'>
               <img src='https://media.cntraveler.com/photos/5ea883674e5fff00083ccef1/16:9/w_3599,h_2024,c_limit/Safari-GettyImages-143917249.jpg' alt='pack'/>
               <button>Buy</button>
@@ -19,6 +20,8 @@ function App() {
               <button>Buy</button>
             </div>
           </div>
+
+
         </div>
       </header>
     </div>
