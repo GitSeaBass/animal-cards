ec105819e18e1046e49bf0eddb00a99febc2eb26
Sebastian Garcia <sag44402@uga.edu>
2023-07-18 10:14:10 -0400

    currency state and header links

--- Front/animal-cards/src/components/Header.js
+++ Front/animal-cards/src/components/Header.js
@@ -1,13 +1,20 @@
 import './Header.css';
+import { Link } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
     return (
         <>
             <div className='header'>
-                <h3>Store</h3>
-                <h3>Collection</h3>
-                <h3>Extra</h3>
-                <h3>Currency</h3>
+                <Link to={'/store'}>
+                    <h3>Store</h3>
+                </Link>      
+                <Link>
+                    <h3>Collection</h3>
+                </Link>
+                <Link>
+                    <h3>Extra</h3>
+                </Link>
+                <h3 id='value'>{props.currency}</h3>
             </div>
         </>
     );
--- Front/animal-cards/src/components/Main.js
+++ Front/animal-cards/src/components/Main.js
@@ -1,11 +1,31 @@
 import Header from './Header';
 import Banner from './Banner';
+import { useState } from 'react';
 
 function Main() {
+
+    const [currency, setCurrency] = useState(0);
+
+    const addCurrency = (val) => {
+        setCurrency(() => {
+            if (currency + val >= 0) {
+                return currency + val;
+            } else {
+                return currency;
+            }
+        })
+    }
+
     return (
         <>
-            <Header />
+            <Header currency = {currency}/>
             <Banner />
+            <button id='testAddButton' onClick={() => {
+                addCurrency(100);
+            }}>Add 100</button>
+            <button id='testSubButton' onClick={() => {
+                addCurrency(-100);
+            }}>Subtract 100</button>
         </>
     );
 }
